e7ac36c15b9e348dd8a5ca5680cb678d3aea614f
Euclides Araujo <araujojr@gmail.com>
2019-05-04 22:34:53 -0400

    Refactoring Gun 2
    Create and attach Gun Component to First player char

--- Source/TestingGrounds/Player/FirstPersonCharacter.h
+++ Source/TestingGrounds/Player/FirstPersonCharacter.h
@@ -52,6 +52,13 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
 	uint32 bUsingMotionControllers : 1;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<class AGun> GunBlueprint;
+
+private:
+
+	AGun* Gun;
+
 protected:
 	///** Resets HMD orientation and position in VR. */
 	//void OnResetVR();
@@ -105,6 +112,5 @@ public:
 	FORCEINLINE class USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
 	/** Returns FirstPersonCameraComponent subobject **/
 	FORCEINLINE class UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
-
 };
 
